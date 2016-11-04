9f2655b836fbaea3b90c71922a8e088d441a88d5
Professor Touré <prof.toure@hodaritcmachine>
2016-11-04 11:30:19 -0700

    Initial Commit

---
+++ pokemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemon
+//
+//  Created by Professor Touré on 11/4/16.
+//  Copyright © 2016 Professor Touré. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
